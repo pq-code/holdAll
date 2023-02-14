11f37c561fa3673b16078f8f6a77a33f730f67cb
pq <68978372+pq-code@users.noreply.github.com>
2023-02-14 15:40:20 +0800

    存档

---
+++ dist/assets/index-a502be26.js
@@ -0,0 +1,1 @@
+import{_ as o,y as r,a as n,x as t}from"./index-cdb2d9c2.js";const c={};function s(_,a){const e=t("router-view");return n(),r(e)}const i=o(c,[["render",s]]);export{i as default};
---
+++ vite.config.ts
@@ -0,0 +1,97 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
+import compressPlugin from "vite-plugin-compression"; //静态资源压缩
+
+// https://vitejs.dev/config/
+
+export default defineConfig(({ command }) => {
+    return {
+        plugins: [
+            vue(),
+            vueJsx(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+            compressPlugin({
+                ext: ".gz",
+                verbose: true,
+                disable: false,
+                threshold: 1024,
+                algorithm: "gzip",
+                deleteOriginFile: command !== "serve", // 是否删除原始文件
+            }),
+        ],
+        build: {
+            chunkSizeWarningLimit: 1500,
+        },
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+        css: {
+            // css预处理器
+            preprocessorOptions: {
+                less: {
+                    javascriptEnabled: true,
+                    lessOptions: {
+                        modifyVars: {
+                            "primary-color": "#ec6800",
+                        },
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+        },
+    };
+});
+// export default defineConfig({
+//     plugins: [
+//         vue(),
+//         vueJsx(),
+//         AutoImport({
+//             resolvers: [ElementPlusResolver()],
+//         }),
+//         Components({
+//             resolvers: [ElementPlusResolver()],
+//         }),
+//         compressPlugin({
+//             ext: ".gz",
+//             verbose: true,
+//             disable: false,
+//             threshold: 10240,
+//             algorithm: "gzip",
+//             deleteOriginFile: command !== "serve", // 是否删除原始文件
+//         }),
+//     ],
+//     resolve: {
+//         alias: {
+//             "@": fileURLToPath(new URL("./src", import.meta.url)),
+//         },
+//     },
+//     css: {
+//         // css预处理器
+//         preprocessorOptions: {
+//             less: {
+//                 javascriptEnabled: true,
+//                 lessOptions: {
+//                     modifyVars: {
+//                         "primary-color": "#ec6800",
+//                     },
+//                     javascriptEnabled: true,
+//                 },
+//             },
+//         },
+//     },
+// });
